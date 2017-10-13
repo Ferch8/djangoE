131b2c8a42efd16df82ce5a47e0eef1cb38eae3e
Fernando Reyes <f-er1016@hotmail.com>
2017-10-13 17:48:39 -0600

    login

--- blog/views.py
+++ blog/views.py
@@ -4,16 +4,19 @@ from django.utils import timezone
 from .models import Publicacion
 from .forms import PublicacionForm
 from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def listar_pub(request):
     pub = Publicacion.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
     return render(request, 'blog/listar_pub.html', {'pub':pub})
 
+@login_required
 def detalle_pub(request, pk):
     p = get_object_or_404(Publicacion, pk=pk)
     return render(request, 'blog/detalle_publicacion.html', {'p': p})
 
+@login_required
 def post_new(request):
     if request.method == "POST":
         form = PublicacionForm(request.POST)
@@ -26,6 +29,7 @@ def post_new(request):
         form = PublicacionForm()
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_edit(request, pk):
     post = get_object_or_404(Publicacion, pk=pk)
     if request.method == "POST":
@@ -39,15 +43,18 @@ def post_edit(request, pk):
         form = PublicacionForm(instance=post)
     return render(request, 'blog/post_edit.html', {'form': form})
 
+@login_required
 def post_draft_list(request):
     posts = Publicacion.objects.filter(fecha_publicacion__isnull=True).order_by('fecha_creacion')
     return render(request, 'blog/post_draft_list.html', {'posts': posts})
 
+@login_required
 def post_publish(request, pk):
     post = get_object_or_404(Publicacion, pk=pk)
     post.publish()
     return redirect('postear', pk=pk)
 
+@login_required
 def post_remove(request, pk):
     post = get_object_or_404(Publicacion, pk=pk)
     post.delete()
